70b985a9813a38cd890da6343ebed8f60184b9df
sudhanshu-jha <jha.sudhanshu1991@gmail.com>
2021-01-05 20:56:55 +0530

    initial

---
+++ elasticsearchapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import BlogPost
+# Register your models here.
+
+# Need to register my BlogPost so it shows up in the admin
+admin.site.register(BlogPost)
